8f444530fae3b3fb0117fe782a92e6567cf2258d
Parth Dasawant <80618762+parthdasawant@users.noreply.github.com>
2022-10-01 00:27:34 +0530

    logging added

--- app.py
+++ app.py
@@ -1,41 +1,38 @@
-import numpy as np
-# from bidict import bidict
 from flask import (
     Flask, render_template, request,
     redirect, url_for, session
 )
 from random import choice
 # from tensorflow import keras
-from flask import Flask, render_template, Response, request
-import cv2
-import datetime, time
-import os, sys
 import numpy as np
-from threading import Thread
+import logging
 
-global capture,rec_frame, grey, switch, neg, face, rec, out 
-capture=0
-grey=0
-neg=0
-face=0
-switch=1
-rec=0
 
 app = Flask(__name__)
 
+#logging for 
+logging.basicConfig(level = logging.DEBUG, filename ="log.log", filemode="a", format = "%(asctime)s - %(levelname)s - %(message)s" ) 
+logger = logging.getLogger (__name__)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s' )
 
+
+app = Flask(__name__)
 @app.route("/")
 def index():
+    app.logger.info('Index.html page working')
     return render_template("index.html")
 
 @app.route("/about.html")
 def about():
+    app.logger.info('about.html page working')
     return render_template("about.html")
 
 @app.route("/project.html")
 def project():
+    app.logger.info('project.html page working')
+    app.logger.critical('Haven\'t implemented yet')
     return render_template("project.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
